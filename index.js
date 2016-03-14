8e975b5d33a4ade61c4a6bf854997b17e313459d
Vishnu Bharathi <vishnubharathi04@gmail.com>
2016-03-14 16:09:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require("express"),
     app = express();
 
 app.get("/", function (req, res) {
-  res.send("Hey buddy!");
+  res.send("Hey buddy! branch is eb-deploy");
 });
 
 app.listen(process.env.PORT || 3000 , function () {
